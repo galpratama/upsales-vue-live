0d359a5e980c6685a316a0e300a27cd7a72afbf5
Galih Pratama <hanamura.iost@gmail.com>
2023-06-06 22:12:10 +0700

    Pertemuan 8

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,30 +3,42 @@ import HomeView from '@/views/HomeView.vue'
 import SignUpView from '@/views/Auth/SignUpView.vue'
 import AddProductView from '@/views/Auth/AddProductView.vue'
 import AddProductPhotoView from '@/views/Auth/AddProductPhotoView.vue'
+import DashboardView from '@/views/Dashboard/DashboardView.vue'
+import ProductIndexView from '@/views/Dashboard/Product/ProductIndexView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-        path: '/',
-        name: 'home',
-        component: HomeView
+      path: '/',
+      name: 'home',
+      component: HomeView
     },
     {
-        path: '/signup',
-        name: 'signup',
-        component: SignUpView
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView
     },
     {
-        path: '/add-product',
-        name: 'add-product',
-        component: AddProductView
-      },
-    {
-        path: '/add-product-photo',
-        name: 'add-product-photo',
-        component: AddProductPhotoView
+      path: '/add-product',
+      name: 'add-product',
+      component: AddProductView
     },
+    {
+      path: '/add-product-photo',
+      name: 'add-product-photo',
+      component: AddProductPhotoView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    },
+    {
+      path: '/dashboard/products',
+      name: 'dashboard-product-index',
+      component: ProductIndexView
+    }
   ]
 })
 
