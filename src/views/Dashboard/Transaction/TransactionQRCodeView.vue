479a77ecce35d72f30a8291ee74b0316c4cfbe8d
Galih Pratama <hanamura.iost@gmail.com>
2023-06-30 11:42:09 +0700

    transaction pay qr

--- src/views/Dashboard/Transaction/TransactionQRCodeView.vue
+++ src/views/Dashboard/Transaction/TransactionQRCodeView.vue
@@ -1,4 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+import axios from 'axios'
+import type Transaction from '@/types/transaction'
+const API_URL = import.meta.env.VITE_API_URL
+
+const route = useRoute()
+
+const transaction = ref<Transaction>({})
+
+onMounted(() => {
+  fetchTransaction()
+})
+
+async function fetchTransaction(): Promise<void> {
+  // Send request to API
+  try {
+    const response = await axios.get(API_URL + '/transaction/', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+      },
+      params: {
+        id: route.params.id
+      }
+    })
+    transaction.value = response.data.result
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
 <template>
   <div class="flex min-h-screen pb-10">
     <div
@@ -6,7 +38,7 @@
       id="warningSection"
     >
       <img
-        src="@/assets/images/barcode-example.png"
+        :src="transaction.payment_url"
         class="mb-[35px] max-w-[237px] h-auto w-full mt-auto"
         alt=""
       />
@@ -15,7 +47,7 @@
         Continue to get your great items today that makes you happy
       </p>
       <RouterLink
-        :to="{ name: 'dashboard-transaction-detail', params: { id: 1 } }"
+        :to="{ name: 'dashboard-transaction-detail', params: { id: route.params.id } }"
         class="btn-primary md:px-12"
       >
         Done
